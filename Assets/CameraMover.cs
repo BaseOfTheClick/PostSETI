3afc163abbcad4b51806f6749047e449cbe0a8a0
Jesse Hamburger <jesselhamburger@gmail.com>
2014-10-23 11:39:15 -0700

    Redoing the GalaxyGenerator
    Also working on the way the planet and GUI's position relative to the
    galaxy is controlled. Should make things easier and cleaner.

---
+++ Assets/CameraMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+	public float zoomOutRate;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3 (transform.position.x,
+		                                  transform.position.y,
+		                                  transform.position.z + zoomOutRate * Time.deltaTime);
+
+	}
+
+}
