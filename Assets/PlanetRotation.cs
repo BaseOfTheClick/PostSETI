fe536f25d8b00f6754b68a4bba36bd3409e0adcc
Jesse Hamburger <jesselhamburger@gmail.com>
2014-10-23 23:08:45 -0700

    got phase 1 of galactic generator working
    Needs heavy optimization. As is, it currently generates an average of
    ~24,672,376 lens flares when you can see the whole galaxy, which is
    obviously problematic.
    
    However, it is generating stars based on the camera's view, which is
    cool.
    
    Next up:
    -Choose from a set of prefabs that includes stars, star clusters,
    nebulae, etc. when generating. (must remember to enable mipmaps for
    those guys)
    -After that is combining some of those objects as they get farther away.
    
    THAT will be the hard part. Probably going to involve Rendertextures,
    which I don't know how to use yet.

---
+++ Assets/PlanetRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetRotation : MonoBehaviour {
+
+	public float RotationRate = 10.0f;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.RotateAround ( transform.position, transform.up, RotationRate * Time.deltaTime );
+
+	}
+}
---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+	public int zoomOutRate = 3;
+
+	// Use this for initialization
+	void Start () {
+		// Make sure the camera moves backwards.
+		// if the camera is on the negative side of the z-axis
+		// set zoomOutRate to be negative.
+		if ( Mathf.Sign(transform.position.z) == -1 ) {
+			zoomOutRate = zoomOutRate * -1;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = transform.position + new Vector3( 0.0f, 0.0f, zoomOutRate * Time.deltaTime );
+
+	}
+
+}
